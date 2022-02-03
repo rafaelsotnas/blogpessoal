babc760bc0f7bc00753b7a1074bbdd35473201db
Rafael <rafaelferreira.0096@gmail.com>
2022-02-03 15:19:04 -0300

    Deploy blog pessoal

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name="tb_usuarios")
 public class Usuario {
@@ -26,6 +28,7 @@ public class Usuario {
 	@NotNull(message="A inserção do nome é obrigatória.")
 	private String nome;
 
+	@Schema(example="email@email.com.br")
 	@NotNull(message="A inserção do usuário é obrigatória.")
 	@Email(message="O usuário deve conter um e-mail válido.")
 	private String usuario;
